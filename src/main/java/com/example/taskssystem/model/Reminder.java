106bc102009a1b955a17d6bf5b2143488679b2b7
Alin Ioan Chiperi <alin-ioan.chiperi@endava.com>
2023-10-04 11:18:27 +0300

    create users and tasks

---
+++ src/main/java/com/example/taskssystem/model/Reminder.java
@@ -0,0 +1,30 @@
+package com.example.taskssystem.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "reminders")
+public class Reminder {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "task_id", nullable = false)
+    private Task task;
+
+    @Column(nullable = false)
+    private Date reminderDateTime;
+
+    @Column(nullable = false)
+    private boolean sent;
+}
---
+++ src/main/java/com/example/taskssystem/model/Task.java
@@ -0,0 +1,34 @@
+package com.example.taskssystem.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+
+@Entity
+@Table(name = "tasks")
+@Getter
+@Setter
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String title;
+    private String description;
+    private Date dueDate;
+    private TaskStatus taskStatus;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}
