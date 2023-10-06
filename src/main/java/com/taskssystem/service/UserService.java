58f26b45f4c134898c2a6894450b723103660bb6
Alin Ioan Chiperi <alin-ioan.chiperi@endava.com>
2023-10-06 09:30:41 +0300

    Create security for app

---
+++ src/main/java/com/taskssystem/service/UserService.java
@@ -0,0 +1,24 @@
+package com.taskssystem.service;
+
+import com.taskssystem.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+    private final PasswordEncoder encoder;
+    private final UserRepository userRepository;
+
+    public UserService(PasswordEncoder encoder, UserRepository userRepository) {
+        this.encoder = encoder;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
---
+++ src/main/java/com/taskssystem/utils/KeyGenerator.java
@@ -0,0 +1,19 @@
+package com.taskssystem.utils;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+
+public class KeyGenerator {
+    public static KeyPair generateRsaKey() {
+        KeyPair keyPair;
+        try {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048);
+            keyPair = keyPairGenerator.generateKeyPair();
+        } catch (NoSuchAlgorithmException e) {
+            throw new IllegalArgumentException("No Such Algorithm Exception");
+        }
+        return keyPair;
+    }
+}
